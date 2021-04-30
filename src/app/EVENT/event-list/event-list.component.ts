0e7ceec8424fc5346bd9127908a9aba9f8839663
Astthor <a.bragason@gmail.com>
2021-04-30 16:01:55 +0200

    Added to event and event-list components and respective html files. Nothing showing yet as missing eeventReducer class for redux

--- src/app/EVENT/event-list/event-list.component.ts
+++ src/app/EVENT/event-list/event-list.component.ts
@@ -1,4 +1,8 @@
+import { NgRedux } from '@angular-redux/store';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { EventActions } from 'src/app/redux/actions/eventActions';
+import { AppState } from 'src/app/redux/state/appState';
 import { Event } from '../../entities/event';
 import { EventService } from '../../service/event.service'; 
 
@@ -9,9 +13,24 @@ import { EventService } from '../../service/event.service';
 })
 export class EventListComponent implements OnInit {
 
-  constructor(private eventService: EventService) { }
+
+  events: Event[];
+  displayedColumns: String[] = ['title', 'startDate', 'location', 'status', 'edit'];
+  
+  constructor(private eventService: EventService, private router: Router,
+              private ngRedux: NgRedux<AppState>, private eventActions: EventActions) { }
 
   ngOnInit(): void {
+    this.ngRedux.select(state => state.eventsList).subscribe(Response => {
+      this.events = Response.eventList;
+    })
   }
 
+  timeForTable(date: Date): String {
+    return date.getDate() + '/' + date.getMonth() + 1 + '/' + date.getFullYear();
+  }
+  viewEvent(viewedEvent: Event): void{}
+  editEvent(editableEvent: Event): void {}
+  deleteEvent(event: Event): void {}
+  createEvent(): void {}
 }
--- src/app/EVENT/event/event.component.ts
+++ src/app/EVENT/event/event.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { Event } from 'src/app/entities/event';
+
 
 @Component({
   selector: 'app-event',
@@ -6,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./event.component.css']
 })
 export class EventComponent implements OnInit {
-
+  
+  @Input() event: Event;
+  @Output() eventClicked: EventEmitter<Event> = new EventEmitter<Event>();
   constructor() { }
 
   ngOnInit(): void {
